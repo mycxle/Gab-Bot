70e3bc0c34fa969258f94deb76506a68ee9ab7bb
Michael Webb <itsyourpalmike@gmail.com>
2018-08-06 14:23:47 -0700

    ...

--- gab.py
+++ gab.py
@@ -17,7 +17,16 @@ login_url = "https://gab.ai/auth/login"
 username = os.environ['username']
 password = os.environ['password']
 
-browser = webdriver.Chrome()
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+
+chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_bin
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument('headless')
+options.add_argument('window-size=1200x600')
+browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
 browser.get(login_url)
 
